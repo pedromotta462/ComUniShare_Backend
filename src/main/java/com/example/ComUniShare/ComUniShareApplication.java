e340ac24412ad6e917735ebf39499f3513b19024
pedromotta462 <pedromotta1d2015@gmail.com>
2023-11-07 15:01:39 -0300

    first commit

---
+++ src/main/java/com/example/ComUniShare/ComUniShareApplication.java
@@ -0,0 +1,13 @@
+package com.example.ComUniShare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComUniShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComUniShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ComUniShare/ComUniShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ComUniShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComUniShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
